6b25827d3e28b8ed6361b26dd35141c31a78dcb7
Halanadiniz <128141932+Halanadiniz@users.noreply.github.com>
2023-10-09 17:01:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function contas(){
+    var a = vara.value;
+    var b = varb.value;
+    var c = varc.value;
+
+    var resposta = b+a;
+     
+    if(a == 0){
+        alert("a deve ser diferente de 0");
+    }
+    else{
+        if(c == resposta){
+            alert("acertou");
+        }
+    }
+        else{
+            alert("errou");
+        }
+}
