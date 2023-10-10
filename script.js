6d2f7c9c0c67465fa0a8e671417be8b347d54ef4
Halanadiniz <128141932+Halanadiniz@users.noreply.github.com>
2023-10-10 10:27:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function equacao(){
     var b = varb.value;
     var c = varc.value;
 
-    var resposta = a*b;
+    var resposta = a/b;
      
     if(a == 0){
         alert("A deve ser diferente de 0");
